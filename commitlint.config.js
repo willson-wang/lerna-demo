85cce377ca6d35b57273b2c4239545cfbaa26463
wangks <wangks@mingyuanyun.com>
2021-07-15 20:18:46 +0800

    feat: 减少随机数

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,20 +2,5 @@ module.exports = {
   // 继承默认配置
   extends: [
     '@commitlint/config-conventional',
-  ],
-  // 自定义规则
-  rules: {
-    'type-enum': [2, 'always', [
-      'upd',
-      'feat',
-      'fix',
-      'refactor',
-      'docs',
-      'chore',
-      'style',
-      'revert',
-      'build',
-    ]],
-    'header-max-length': [0, 'always', 72],
-  },
+  ]
 };
