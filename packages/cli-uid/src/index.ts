02538514c72df47db8a166e0807ed8f32f534828
wangks <wangks@mingyuanyun.com>
2021-07-15 20:00:40 +0800

    fix(cli-uid): 修复bug

--- packages/cli-uid/src/index.ts
+++ packages/cli-uid/src/index.ts
@@ -1,4 +1,4 @@
-const arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+const arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', '1', '2', '3', '4', '5', '6', '7', '8']
 
 export function getUid(): string {
   return arr[Math.round((Math.random() * arr.length))]
